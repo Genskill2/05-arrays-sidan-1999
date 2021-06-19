5f10df10ec1682a14cafb38358364f628c572210
sidan-1999 <70771908+sidan-1999@users.noreply.github.com>
2021-06-19 20:27:07 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,12 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+float average(int k[], int n){
+    float sum=0.00;
+    for (int i=0;i<n;i++){
+        sum+=k[i];
+    }
+    float result=sum/n ;
+    return result;
+}
+
