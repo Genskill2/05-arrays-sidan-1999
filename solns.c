d4899d47be48c3edb771adf02afd924a465527e0
sidan-1999 <70771908+sidan-1999@users.noreply.github.com>
2021-06-19 20:39:47 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,6 +1,18 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
 
+int max(int k[], int n){
+    int maximum=k[0];
+    for (int i=0;i<n;i++){
+        if (k[i]>maximum){
+            maximum=k[i];
+            
+        }
+    }
+    return maximum;
+    
+}
+
 float average(int k[], int n){
     float sum=0.00;
     for (int i=0;i<n;i++){
